5661dacce8b7b4d4ed9e1dd6f00bec97eed8400f
nemmtor <kacper0witas@gmail.com>
2025-08-17 22:21:32 +0200

    fix: get core instance inside component

--- packages/injectio-react/src/injectio.ts
+++ packages/injectio-react/src/injectio.ts
@@ -1,12 +1,11 @@
-import { useSyncExternalStore, createElement } from "react";
-import { Core } from "./internal/core.js";
-import { InjectedComponent } from "./internal/injected-component.js";
-
-const core = Core.getInstance();
+import { useSyncExternalStore, createElement } from 'react';
+import { Core } from './internal/core.js';
+import { InjectedComponent } from './internal/injected-component.js';
 
 export const Injectio = () => {
+  const core = Core.getInstance();
   const snapshot = useSyncExternalStore(core.subscribe, core.getSnapshot);
   return snapshot.map((item) =>
-    createElement(InjectedComponent, { key: item.id, item })
+    createElement(InjectedComponent, { key: item.id, item }),
   );
 };
